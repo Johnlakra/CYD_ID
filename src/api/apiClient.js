6c2319b55ee583dafe31fb25539f72d922872c85
John Lakra <johnamanlakra000@gmail.com>
2025-08-11 14:21:04 +0530

    api client

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const baseURL = 'https://cydidbe-production.up.railway.app';
-export const baseURL = 'http://localhost:3000';
+export const baseURL = 'https://cydidbe-production.up.railway.app';
+// export const baseURL = 'http://localhost:3000';
 // Create axios instance
 const apiClient = axios.create({
   baseURL: baseURL,
